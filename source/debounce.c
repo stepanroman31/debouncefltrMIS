82bd5ed679ccc566c3c973b5abf81488e0c88b28
Štěpán <tpn@DESKTOP-61B1L44.mshome.net>
2025-10-28 17:28:45 +0100

    první funkce

--- source/debounce.c
+++ source/debounce.c
@@ -1,10 +1,23 @@
 #include "debounce.h"
 
-
-void initFilterTypeBool (filterTypeBool_t *filter, initValue){
+// Funkce filtru
+void initFilterTypeBool (filterTypeBool_t *filter, bool initValue){
     filter->counter = 0;
     filter->previousValue = initValue;
     filter->currentValue = initValue;
+}
+// Nastavení po?áte?ní hodnot
+void initMemoryTypeBool (memoryTypeBool_t *mem, bool initValue){
+    mem->previousInputValue = false;
+    mem->outputValue = initValue;
+}
+//Funkce pam?ti
+bool runMemoryTypeBool (memoryTypeBool_t *mem, bool value){
+    if (value == true && mem->previousInputValue == false){
+        mem->outputValue = !mem->outputValue;
+    }
+    mem->previousInputValue = value;
+    return mem->outputValue;
 }
 
        
