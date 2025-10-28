0dbd2213311fd5fdc2eca0a6284345b1d018f1d4
Štěpán <tpn@DESKTOP-61B1L44.mshome.net>
2025-10-28 17:21:24 +0100

    opravy

--- source/debounce.c
+++ source/debounce.c
@@ -1,5 +1,12 @@
 #include "debounce.h"
 
+
+void initFilterTypeBool (filterTypeBool_t *filter, initValue){
+    filter->counter = 0;
+    filter->previousValue = initValue;
+    filter->currentValue = initValue;
+}
+
        
 
     
