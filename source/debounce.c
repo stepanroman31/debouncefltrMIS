47d2d519a4403bd76cb9a2ffc651dbe2eda1852b
Štěpán <tpn@DESKTOP-61B1L44.mshome.net>
2025-10-28 17:43:01 +0100

    Doladění plus popis programu

--- source/debounce.c
+++ source/debounce.c
@@ -6,12 +6,12 @@ void initFilterTypeBool (filterTypeBool_t *filter, bool initValue){
     filter->previousValue = initValue;
     filter->currentValue = initValue;
 }
-// Nastavení po?áte?ní hodnot
+// Nastavení po?áte?ních hodnot
 void initMemoryTypeBool (memoryTypeBool_t *mem, bool initValue){
     mem->previousInputValue = false;
     mem->outputValue = initValue;
 }
-//Funkce pam?ti
+//Logika pam?ti
 bool runMemoryTypeBool (memoryTypeBool_t *mem, bool value){
     if (value == true && mem->previousInputValue == false){
         mem->outputValue = !mem->outputValue;
@@ -19,16 +19,18 @@ bool runMemoryTypeBool (memoryTypeBool_t *mem, bool value){
     mem->previousInputValue = value;
     return mem->outputValue;
 }
+// Detekce zm?ny
 bool runFilterTypeBool (filterTypeBool_t *filter, bool value){
     if (value != filter->previousValue){
         filter->counter++;
+        // Po?et cykl? pot?ebných pro zm?nu stavu LED
         if (filter->counter >= 3){
             filter->previousValue = value;
             filter->currentValue = value;
             filter->counter = 0;
         }
     }else {
-        filter->counter = 0;
+        filter->counter = 0;    // Reset ?íta?e
     }
     return filter->currentValue;
 }
