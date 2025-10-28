0abf26a08c6af359becec8be266848dca24fa038
Štěpán <tpn@DESKTOP-61B1L44.mshome.net>
2025-10-28 17:50:27 +0100

    uprava popisku

--- source/debounce.c
+++ source/debounce.c
@@ -6,12 +6,12 @@ void initFilterTypeBool (filterTypeBool_t *filter, bool initValue){
     filter->previousValue = initValue;
     filter->currentValue = initValue;
 }
-// Nastavení po?áte?ních hodnot
+// Nastavení pocatecnich hodnot
 void initMemoryTypeBool (memoryTypeBool_t *mem, bool initValue){
     mem->previousInputValue = false;
     mem->outputValue = initValue;
 }
-//Logika pam?ti
+//Logika pameti
 bool runMemoryTypeBool (memoryTypeBool_t *mem, bool value){
     if (value == true && mem->previousInputValue == false){
         mem->outputValue = !mem->outputValue;
@@ -19,18 +19,18 @@ bool runMemoryTypeBool (memoryTypeBool_t *mem, bool value){
     mem->previousInputValue = value;
     return mem->outputValue;
 }
-// Detekce zm?ny
+// Detekce zmeny
 bool runFilterTypeBool (filterTypeBool_t *filter, bool value){
     if (value != filter->previousValue){
         filter->counter++;
-        // Po?et cykl? pot?ebných pro zm?nu stavu LED
+        // Pocet cyklu potrebných pro zmenu stavu LED
         if (filter->counter >= 3){
             filter->previousValue = value;
             filter->currentValue = value;
             filter->counter = 0;
         }
     }else {
-        filter->counter = 0;    // Reset ?íta?e
+        filter->counter = 0;    // Reset cítace
     }
     return filter->currentValue;
 }
